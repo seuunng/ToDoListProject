55398bb4b276f3c080460f6f81f16e2c1e27188e
SeungHee <mnb2856@gmail.com>
2024-06-10 09:21:29 +0900

    monthly board 디자인

--- src/views/boards/MonthlyBoard.vue
+++ src/views/boards/MonthlyBoard.vue
@@ -1,6 +1,33 @@
 <template>
     <div class="MonthlyBoard">
         <h4>MonthlyBoard</h4>
+        <div class="carlendar">
+            <table>
+                <thead>
+                    <tr>
+                        <td>월</td>
+                        <td>화</td>
+                        <td>수</td>
+                        <td>목</td>
+                        <td>금</td>
+                        <td>토</td>
+                        <td>일</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </div>
     </div>
 </template>
 
