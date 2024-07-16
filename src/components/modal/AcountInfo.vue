fd2cfa51a3d612e62dbe4bca1bf87e30f6647c36
seuunng <155802305+seuunng@users.noreply.github.com>
2024-07-16 23:53:10 +0900

    acountInfo 컴포넌트 수정

--- src/components/modal/AcountInfo.vue
+++ src/components/modal/AcountInfo.vue
@@ -10,7 +10,8 @@
     <div class="modal-dialog modal-dialog-centered">
       <div class="modal-content">
         <div class="modal-header">
-          <h4>AcountInfo</h4>
+          <div v-if="!isEditing"><h4>AcountInfo</h4></div>
+          <div v-else><h4>Login</h4></div>
           <button
             type="button"
             class="btn-close"
@@ -23,15 +24,18 @@
             <div>
               <div class="d-flex align-items-center line">
                 <h5>이메일</h5>
-                {{ id }}
+                <div v-if="!isEditing">{{ id }}</div>
+                <div v-else><input v-model="editableId" /></div>
               </div>
               <div class="d-flex align-items-center line">
                 <h5>닉네임</h5>
-                {{ nickname }}
+                <div v-if="!isEditing">{{ nickname }}</div>
+                <div v-else><input v-model="editableNickname" /></div>
               </div>
               <div class="d-flex align-items-center line">
                 <h5>가입일</h5>
-                {{ created_at }}
+                <div v-if="!isEditing">{{ created_at }}</div>
+                <div v-else><input v-model="editableCreatedAt" /></div>
               </div>
               <div class="d-flex align-items-center line">
                 <h5>간편로그인설정</h5>
@@ -44,11 +48,11 @@
             </div>
             <hr />
             <div class="footer d-flex align-items-center line">
-              <span><h5>회원가입</h5></span>
-            <hr />
-              <span><h5>로그인</h5> </span>
-              <span><h5>로그아웃</h5> </span>
-              <span><h5>회원탈퇴</h5></span>
+              <span><h5 v-if="!isLoggedIn && isGuest">{{ '회원가입' }}</h5></span>
+              <span v-if="!isLoggedIn && isGuest"><h5>게스트 로그인</h5></span>
+              <span v-if="!isLoggedIn && !isGuest"><h5 @click="toggleEdit">{{ isEditing ? '저장' : '로그인' }}</h5></span>
+              <span v-if="isLoggedIn"><h5>로그아웃</h5></span>
+              <span v-if="isLoggedIn"><h5>회원탈퇴</h5></span>
             </div>
           </div>
         </div>
@@ -72,14 +76,27 @@ export default {
     nickname: String,
     created_at: String,
   },
-  setup() {
+  setup(props) {
     const acountInfoModal = ref(null);
+    const isEditing = ref(false);
+    const editableId = ref(props.id);
+    const editableNickname = ref(props.nickname);
+    const editableCreatedAt = ref(props.created_at);
+    // Additional states for account status
+    const isLoggedIn = ref(false); // This should be set based on actual account status
+    const isGuest = ref(false); // This should be set based on actual account status
 
     const showModal = () => {
       const modal = new bootstrap.Modal(acountInfoModal.value);
       modal.show();
     };
-
+    const toggleEdit = () => {
+      if (isEditing.value) {
+        // Save the changes if switching from edit to view mode
+        // Here you might want to emit events or handle API calls to save changes
+      }
+      isEditing.value = !isEditing.value;
+    };
     onMounted(() => {
       if (acountInfoModal.value) {
         acountInfoModal.value.showModal = showModal;
@@ -89,6 +106,13 @@ export default {
     return {
       acountInfoModal,
       showModal,
+      isEditing,
+      editableId,
+      editableNickname,
+      editableCreatedAt,
+      toggleEdit,
+      isLoggedIn,
+      isGuest,
     };
   },
 };
